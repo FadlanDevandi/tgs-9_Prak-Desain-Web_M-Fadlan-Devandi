4c10bbe2d001be0df4222cd7505e4318172945e6
FadlanDevandi <fadlandevandi@gmail.com>
2024-11-20 16:18:16 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF0000', // Warna merah
+        secondary: '#008000', // Warna hijau
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'], // Mengganti font dengan Roboto
+      },
+    },
+  },
+  plugins: [],
+}
